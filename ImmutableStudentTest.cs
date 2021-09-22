487678a823d11472e1313ce460ae7d978252aa8d
aljb <aljb@itu.dk>
2021-09-22 13:20:04 +0200

    Implemented record and tested record properties
    Co-authored-by: havlebbe <havlebbe@users.noreply.github.com>
    Co-authored-by: Slas1 <Slas1@users.noreply.github.com>

--- ImmutableStudentTest.cs
+++ ImmutableStudentTest.cs
@@ -5,10 +5,51 @@ namespace Assignment2
 {
     public class ImmutableStudentTest
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(1, "Per", "Olsen", 2019, 01, 30, 2022, 05, 10)]
+        [InlineData(99, "Hans", "Steffenssen", 2009, 12, 24, 2012, 1, 1)]
+        public void Record_equality_comparer_shows_equal(int id, string firstname, string surname, int yearStart, int monthStart, int dayStart, int yearEnd, int monthEnd, int dayEnd)
         {
+            ImmutableStudent student1 = new ImmutableStudent(id, firstname, surname, new DateTime(yearStart, monthStart, dayStart), new DateTime(yearEnd, monthEnd, dayEnd), new DateTime(yearEnd, monthEnd, dayEnd));
+            ImmutableStudent student2 = new ImmutableStudent(id, firstname, surname, new DateTime(yearStart, monthStart, dayStart), new DateTime(yearEnd, monthEnd, dayEnd), new DateTime(yearEnd, monthEnd, dayEnd));
 
+            Assert.True(student1 == student2);
+        }
+
+        [Theory]
+        [InlineData(1, "Per", "Olsen", 2019, 01, 30, 2022, 05, 10, 
+                    2, "Ole", "Petersen", 2020, 01, 30, 2023, 05, 10)]
+        [InlineData(1, "alice", "alice", 2019, 01, 09, 2023, 08, 23, 
+                    2, "bob", "bob", 2019, 01, 09, 2023, 08, 23)]
+        public void Record_equality_comparer_shows_false(int id1, string givenName1, string surName1, int yearStart1, int monthStart1, int dayStart1, int yearEnd1, int monthEnd1, int dayEnd1, 
+                                                         int id2, string givenName2, string surName2, int yearStart2, int monthStart2, int dayStart2, int yearEnd2, int monthEnd2, int dayEnd2)
+        {
+            DateTime startDate1 = new DateTime(yearStart1, monthStart1, dayStart1);
+            DateTime endDate1 = new DateTime(yearEnd1, monthEnd1, dayEnd1);
+            DateTime gradDate1 = new DateTime(yearEnd1, monthEnd1, dayEnd1);
+            
+            DateTime startDate2 = new DateTime(yearStart2, monthStart2, dayStart2);
+            DateTime endDate2 = new DateTime(yearEnd2, monthEnd2, dayEnd2);
+            DateTime gradDate2 = new DateTime(yearEnd2, monthEnd2, dayEnd2);
+            
+            ImmutableStudent student1 = new ImmutableStudent(id1, givenName1, surName1, startDate1, endDate1, gradDate1);
+            ImmutableStudent student2 = new ImmutableStudent(id2, givenName2, surName2, startDate2, endDate2, gradDate2);
+
+            Assert.False(student1 == student2);
+        }
+
+        [Fact]
+        public void Record_built_in_to_string() 
+        {
+            DateTime startDate = new DateTime(2019, 08, 30);
+            DateTime endDate = new DateTime(2022, 06, 15);
+            DateTime gradDate = new DateTime(2022, 06, 15);
+            
+            ImmutableStudent alice = new ImmutableStudent(1, "John", "Doe", startDate, endDate, gradDate);
+            string expected = "ImmutableStudent { id = 1, GivenName = John, SurName = Doe, StartDate = 30/08/2019 00.00.00, " +
+                              "EndDate = 15/06/2022 00.00.00, GraduationDate = 15/06/2022 00.00.00, Status = Active }";
+            
+            Assert.Equal(expected, alice.ToString());
         }
 
         [Theory]
